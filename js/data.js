5b2f5a36ed2fc7c5f12d02e4b68efd257f2c25bd
StefanoMast <ste.mastrantonio@gmail.com>
2022-05-11 15:59:56 +0200

    add milestone 2

--- js/data.js
+++ js/data.js
@@ -2,6 +2,7 @@ const app = new Vue(
     {
         el: "#root",
         data: {
+            selectedChat: 0,
             contacts: [
                 {
                     name: 'Michele',
@@ -28,21 +29,10 @@ const app = new Vue(
                     avatar: '_3',
                     visible: true,
                     messages: [
-                        {
-                            date: '28/03/2020 10:10:40',
-                            message: 'La Marianna va in campagna',
-                            status: 'received'
+                        {date: '28/03/2020 10:10:40', message: 'La Marianna va in campagna', status: 'received'
                         },
-                        {
-                            date: '28/03/2020 10:20:10',
-                            message: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
-                        },
-                        {
-                            date: '28/03/2020 16:15:22',
-                            message: 'Ah scusa!',
-                            status: 'received'
-                        }
+                        {date: '28/03/2020 10:20:10', message: 'Sicuro di non aver sbagliato chat?', status: 'sent'},
+                        {date: '28/03/2020 16:15:22', message: 'Ah scusa!', status: 'received'}
                     ],
                 },
                 {
@@ -50,15 +40,9 @@ const app = new Vue(
                     avatar: '_4',
                     visible: true,
                     messages: [
-                        {
-                            date: '10/01/2020 15:30:55',
-                            message: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
+                        {date: '10/01/2020 15:30:55', message: 'Lo sai che ha aperto una nuova pizzeria?', status: 'sent'
                         },
-                        {
-                            date: '10/01/2020 15:50:00',
-                            message: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
+                        {date: '10/01/2020 15:50:00', message: 'Si, ma preferirei andare al cinema',status: 'received'
                         }
                     ],
                 },
@@ -67,16 +51,8 @@ const app = new Vue(
                     avatar: '_5',
                     visible: true,
                     messages: [
-                        {
-                            date: '10/01/2020 15:30:55',
-                            message: 'Ricordati di chiamare la nonna',
-                            status: 'sent'
-                        },
-                        {
-                            date: '10/01/2020 15:50:00',
-                            message: 'Va bene, stasera la sento',
-                            status: 'received'
-                        }
+                        {date: '10/01/2020 15:30:55', message: 'Ricordati di chiamare la nonna', status: 'sent'},
+                        {date: '10/01/2020 15:50:00', message: 'Va bene, stasera la sento', status: 'received'}
                     ],
                 },
                 {
@@ -84,21 +60,11 @@ const app = new Vue(
                     avatar: '_6',
                     visible: true,
                     messages: [
-                        {
-                            date: '10/01/2020 15:30:55',
-                            message: 'Ciao Claudia, hai novità?',
-                            status: 'sent'
+                        {date: '10/01/2020 15:30:55', message: 'Ciao Claudia, hai novità?', status: 'sent'
                         },
-                        {
-                            date: '10/01/2020 15:50:00',
-                            message: 'Non ancora',
-                            status: 'received'
+                        {date: '10/01/2020 15:50:00', message: 'Non ancora', status: 'received'
                         },
-                        {
-                            date: '10/01/2020 15:51:00',
-                            message: 'Nessuna nuova, buona nuova',
-                            status: 'sent'
-                        }
+                        {date: '10/01/2020 15:51:00', message: 'Nessuna nuova, buona nuova', status: 'sent'}
                     ],
                 },
                 {
@@ -106,16 +72,8 @@ const app = new Vue(
                     avatar: '_7',
                     visible: true,
                     messages: [
-                        {
-                            date: '10/01/2020 15:30:55',
-                            message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                            status: 'sent'
-                        },
-                        {
-                            date: '10/01/2020 15:50:00',
-                            message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                            status: 'received'
-                        }
+                        {date: '10/01/2020 15:30:55', message: 'Fai gli auguri a Martina che è il suo compleanno!', status: 'sent'},
+                        {date: '10/01/2020 15:50:00', message: 'Grazie per avermelo ricordato, le scrivo subito!', status: 'received'}
                     ],
                 },
                 {
@@ -123,25 +81,19 @@ const app = new Vue(
                     avatar: '_8',
                     visible: true,
                     messages: [
-                        {
-                            date: '10/01/2020 15:30:55',
-                            message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                            status: 'received'
+                        {date: '10/01/2020 15:30:55', message: 'Ciao, andiamo a mangiare la pizza stasera?', status: 'received'},
+                        {date: '10/01/2020 15:50:00', message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!', status: 'sent'
                         },
-                        {
-                            date: '10/01/2020 15:50:00',
-                            message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                            status: 'sent'
-                        },
-                        {
-                            date: '10/01/2020 15:51:00',
-                            message: 'OK!!',
-                            status: 'received'
-                        }
+                        {date: '10/01/2020 15:51:00', message: 'OK!!', status: 'received'}
                     ],
                 }
-            ]
-
+            ],
+        },
+        methods: {
+            //al click sulla chat mostro i messaggi della chat di riferimento
+            changeChat(index) {
+                this.selectedChat = index;
+            }
         }
     }
 );
